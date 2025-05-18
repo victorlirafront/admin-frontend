174313ab4eff5726eadfea137fe7a3c6da9f02a3
Victor Lira <104510669+victorlirafront@users.noreply.github.com>
2025-05-18 17:17:28 -0300

    update dynamic page

--- pages/product/[id].vue
+++ pages/product/[id].vue
@@ -16,12 +16,20 @@ const { data: product } = await useAsyncData('product', async () => {
 </script>
 
 <template>
-  <div v-if="product">
-    <h1>{{ product.name }}</h1>
-    <img :src="product.image" alt="Product Image" />
-    <p>Preço: R$ {{ product.price.toFixed(2) }}</p>
-  </div>
-  <div v-else>
-    <p>Produto não encontrado.</p>
-  </div>
+  <main class="main-products">
+    <div class="products">
+      <div v-if="product" class="product-box-image">
+        <img class="product-image" :src="product.image" alt="Product Image" />
+        <img class="product-image-sheet" src="https://ik.imagekit.io/Victorliradev/loja-virtual-vue/measures-tshirt_p7PT-1yRV.webp?updatedAt=1742744094164" alt="Product Image" />
+      </div>
+      <div class="product-box-info">
+        <h1>{{ product.name }}</h1>
+        <p>Preço: R$ {{ product.price.toFixed(2) }}</p>
+      </div>
+    </div>
+  </main>
 </template>
+
+<style scoped lang="scss">
+@use './product.scss' as *;
+</style>
